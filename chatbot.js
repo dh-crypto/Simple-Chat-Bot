731b74b56393b8fb72525395d0d7b3e82e94ce85
Dharm Patel <71087613+dh-crypto@users.noreply.github.com>
2021-11-23 22:20:17 +0530

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,16 @@
+   <script>
+        
+            var know = {
+               "" : "",
+               
+            };
+            function talk() {
+                var user = document.getElementById("userBox").value;
+                document.getElementById("chatLog").innerHTML += user+"<br>";
+                if (user in know) {
+                    document.getElementById("chatLog").innerHTML += know[user]+"<br>";
+                } else {
+                    document.getElementById("chatLog").innerHTML += "I don't understand... <br>";
+                }
+            }
+        </script>
